da7f77db5c2bf50130049f2dbe77cea39c461574
Xobbit <ben300300@gmail.com>
2022-11-29 18:04:39 +0300

    deleted some data

--- erc/esn/models.py
+++ erc/esn/models.py
@@ -2,32 +2,6 @@ from django.db import models
 from contacts.models import Contacts
 from erc.settings import OBJECT_CHOICES
 
-# class IndividualEntrepreneur(models.Model):
-#     name = models.CharField(max_length=50, null=False, blank=False)
-#     debt = models.DecimalField(max_digits=20, decimal_places=2, default=0, null=True)
-#     contacts = models.ForeignKey(
-#         Contacts,
-#         related_name="contacts_ie",
-#         on_delete=models.CASCADE,
-#         null=True,
-#         blank=True,
-#     )
-#     product = models.ManyToManyField(
-#         "products.Product", related_name="product_ie", null=True
-#     )
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     supplier = models.ForeignKey(
-#         LargeRetailChain,
-#         related_name="supplier_ie",
-#         on_delete=models.CASCADE,
-#         null=True,
-#         blank=True,
-#     )
-#     employees = models.CharField(max_length=50, null=False, blank=False)
-#
-#     class Meta:
-#         verbose_name = "Индивидуальный предприниматель"
-
 
 class ObjectModel(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
