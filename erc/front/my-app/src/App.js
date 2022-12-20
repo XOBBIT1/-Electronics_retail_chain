f5c47aba125466fde74f39188a9efac322087180
Xobbit <ben300300@gmail.com>
2022-12-20 19:08:55 +0300

    added frontend logic

--- erc/front/my-app/src/App.js
+++ erc/front/my-app/src/App.js
@@ -1,12 +1,30 @@
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import axios from 'axios'
+import PostList from './components/BackendJS'
 
 function App() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = () => {
+        axios
+            .get('http://127.0.0.1:8000/contacts')
+            .then((res) => {
+                setPosts(res.data)
+            })
+            .catch((err) => console.log(err))
+    }
+
     return (
-        <div className="App">
+        <div>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <button className="Cleack"> Cleack for copy email</button>
+                <PostList data={posts} />
             </header>
         </div>
     )
